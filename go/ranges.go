7a9ca7329b694df065ed4ae493b76a0561b23b12
alextanhongpin <alextan220990@gmail.com>
2023-07-13 02:03:02 +0800

    chore: update examples

--- go/ranges.go
+++ go/ranges.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/errors"
 	"cuelang.org/go/cue/load"
 )
 
@@ -26,6 +26,9 @@ func main() {
 
 	fmt.Println(method1())
 	fmt.Println(method2())
+	fmt.Println(method3())
+	fmt.Println(method4())
+	fmt.Println(method5())
 }
 
 func method1() error {
@@ -53,15 +56,11 @@ func method2() error {
 		"ranges.cue",
 	}, nil)
 
-	insts, err := ctx.BuildInstances(binst)
-	if err != nil {
-		return err
-	}
-	if len(insts) != 1 {
-		return errors.New("more than 1 instance")
+	v := ctx.BuildInstance(binst[0])
+	if err := v.Err(); err != nil {
+		panic(err)
 	}
 
-	v := insts[0]
 	data := ctx.CompileString(sample, cue.Filename("range.json"))
 	unified := data.Unify(v)
 
@@ -71,6 +70,83 @@ func method2() error {
 		cue.Hidden(true),
 	}
 
-	err = unified.Validate(opts...)
+	err := unified.Validate(opts...)
 	return err
+}
+
+func method3() error {
+	const module = `package ranges
+
+#Range: {
+	min?: *0 | number // 0 if undefined
+	max?: number & >min // must be strictly greater than min if defined.
+}
+`
+	const sample = `#Range & {
+	"min": 10,
+	"max": -10
+}`
+	ctx := cuecontext.New()
+	schema := ctx.CompileString(module, cue.Filename("ranges.cue"))
+	if err := schema.Err(); err != nil {
+		return err
+	}
+
+	prog := ctx.CompileString(sample, cue.Scope(schema), cue.Filename("range.json"))
+	opts := []cue.Option{
+		cue.Attributes(true),
+		cue.Definitions(true),
+		cue.Hidden(true),
+	}
+
+	err := prog.Validate(opts...)
+	return err
+}
+
+func method4() error {
+	const sample = `#Range & {
+	"min": 10,
+	"max": -10
+}`
+	ctx := cuecontext.New()
+	bins := load.Instances([]string{"ranges.v2.cue"}, nil)
+	schema := ctx.BuildInstance(bins[0])
+
+	prog := ctx.CompileString(sample, cue.Scope(schema), cue.Filename("range.json"))
+	opts := []cue.Option{
+		cue.Attributes(true),
+		cue.Definitions(true),
+		cue.Hidden(true),
+	}
+
+	err := prog.Validate(opts...)
+	return err
+}
+
+func method5() error {
+	// Overriding the constraints.
+	const sample = `
+#Min50: #Range & {
+		min: >=50 
+}
+
+#Min50 & {
+	"min": 10,
+	"max": -10
+}`
+	ctx := cuecontext.New()
+	bins := load.Instances([]string{"ranges.v2.cue"}, nil)
+	schema := ctx.BuildInstance(bins[0])
+
+	prog := ctx.CompileString(sample, cue.Scope(schema), cue.Filename("range.json"))
+	opts := []cue.Option{
+		cue.Attributes(true),
+		cue.Definitions(true),
+		cue.Hidden(true),
+	}
+
+	err := prog.Validate(opts...)
+	fmt.Println(errors.Details(err, nil))
+	return err
+
 }

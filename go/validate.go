51d1609d77ffa58fd7a5ae87063df67a2380c92b
Alex Tan Hong Pin <alextan220990@gmail.com>
2024-04-04 02:10:13 +0800

    Create validate.go

---
+++ go/validate.go
@@ -0,0 +1,39 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+
+	"cuelang.org/go/cue"
+	"cuelang.org/go/cue/cuecontext"
+)
+
+func main() {
+	// Create a new context.
+	ctx := cuecontext.New()
+
+	// https://cuelang.org/docs/tour/types/closed/
+	// A closed struct is a struct that has a fixed set of fields.
+	schema := ctx.CompileString(`close({
+		age: >=13
+		name: string 
+		hobby: [...string]
+	})`)
+	// hobby: [string] means hobby is a list of string, but only 1 item
+	// hobby: [string, string] means hobby is a list of string, but only 2 items
+	// hobby: [...string] means hobby is a list of indefinite string
+
+	value := ctx.CompileString(`{
+		"age": 13, 
+		"name": "john",
+		"hobby": ["reading", "coding"]
+	}`)
+	// Compile the schema and value.
+	// Unify the value with the schema.
+	// Validate the unified value.
+	// Concrete(true) means the value must be concrete (required).
+	fmt.Println(schema.Unify(value).Validate(
+		cue.Concrete(true),
+	))
+}
